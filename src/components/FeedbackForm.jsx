a0aae34fcd1ce17711fdd4088ad5092c07cf9f42
mainbsl4 <mainbsl4@gmail.com>
2024-02-24 00:00:12 +0600

    feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,15 +4,27 @@ export default function FeedbackForm() {
   const [score, setScore] = useState("10");
   const [comment, setComment] = useState("");
 
-  const handelChenge = (e) => {
-    setScore(e.target.value);
-    setComment(e.target.value);
+  // const handelChenge = (e) => {
+  //   setScore(e.target.value);
+  //   setComment(e.target.value);
+  // };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <= 5 && comment.length <= 10) {
+      alert("Please provite a comment explain why th eexpreience was poor.");
+      return;
+    }
+    setScore("10");
+    setComment("");
+    console.log(score);
+    console.log(comment);
   };
 
-  console.log(score);
-  console.log(comment);
+  // console.log(score);
+  // console.log(comment);
   return (
-    <form>
+    <form onSubmit={handelSubmit}>
       <div>
         <label htmlFor="score">Score {score}</label>
         <input
@@ -22,14 +34,24 @@ export default function FeedbackForm() {
           min="0"
           max="10"
           value={score}
-          onChange={handelChenge}
+          onChange={(e) => setScore(e.target.value)}
         />
       </div>
       <div>
         <label htmlFor="comment">Your comment</label>
-        <textarea className=" border" name="comment" id="comment" cols="30" rows="2" onChange={handelChenge}></textarea>
+        <textarea
+          className=" border"
+          name="comment"
+          id="comment"
+          cols="30"
+          rows="2"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        ></textarea>
       </div>
-      <button typeof="submit">Submit</button>
+      <button type="submit" disabled={score === "0"}>
+        Submit
+      </button>
     </form>
   );
 }
